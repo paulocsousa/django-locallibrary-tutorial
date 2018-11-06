192f2a2f69c325aa0a62aec37d21260eedb7de34
Paulo Sousa <ps@localhost.com>
2018-11-06 21:43:14 +0000

    Add borrowing relationship between book instances and users

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse # Used to generate URLs by reversing the URL patterns
 import uuid # Required for unique book instances
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -67,6 +68,7 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True) 
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
